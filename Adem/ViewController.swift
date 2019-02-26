03fece7c23478ee507f61cd141329a6d69e4f7eb
ccoats200 <ccoats200@gmail.com>
2019-02-25 21:42:17 -0500

    Initial Commit

---
+++ Adem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adem
+//
+//  Created by Coleman Coats on 2/25/19.
+//  Copyright © 2019 Coleman Coats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
